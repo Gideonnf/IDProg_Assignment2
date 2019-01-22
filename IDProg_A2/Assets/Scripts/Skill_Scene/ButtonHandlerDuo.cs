2104fb7b82c512ab5b6ef37f352c5992a66f0749
JordanTan <174561b@mymail.nyp.edu.sg>
2019-01-22 16:32:00 +0800

    Added Skill scene basic stuff

---
+++ IDProg_A2/Assets/Scripts/Skill_Scene/ButtonHandlerDuo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // required when using UI elements in scripts
+
+public class ButtonHandlerDuo : MonoBehaviour {
+    public Button myButton;
+
+    public Button ButtonAdjacent1;
+    public Button ButtonAdjacent2;
+
+    // Use this for initialization
+    void Start()
+    {
+        myButton.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ IDProg_A2/Assets/Scripts/Skill_Scene/ButtonHandlerOrigin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // required when using UI elements in scripts
+
+public class ButtonHandlerOrigin : MonoBehaviour
+{
+    public Button myButton;
+
+    public Button ButtonUp;
+    public Button ButtonRight;
+    public Button ButtonBottom;
+    public Button ButtonLeft;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetAllAdjacentButtonActive()
+    {
+        ButtonUp.interactable = true;
+        ButtonBottom.interactable = true;
+        ButtonRight.interactable = true;
+        ButtonLeft.interactable = true;       
+    }
+}

b30ee645d3371e2b535a8530548da232d075087c
JamesCrr <g0546699m@gmail.com>
2019-01-24 00:09:03 +0800

    Can Go to MainGame Scene now

---
+++ IDProg_A2/Assets/Scripts/GoToNewScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNewScene : MonoBehaviour {
+
+    // Load new Scene based on Index Passed In
+    // index found from build settings
+    public void GoToNewScene_INT(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // Load new Scene based on Index Passed In
+    // name found from scene Name
+    public void GoToNewScene_STRING(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

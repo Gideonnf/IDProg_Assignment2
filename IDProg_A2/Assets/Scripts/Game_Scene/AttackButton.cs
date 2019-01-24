6132969d645e2908f022070e296fbcb5235ef28b
JamesCrr <g0546699m@gmail.com>
2019-01-24 23:00:54 +0800

    added attack button pressing

---
+++ IDProg_A2/Assets/Scripts/Game_Scene/AttackButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// DOTween
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class AttackButton : MonoBehaviour {
+
+    private float opaqueTimer, maxOpaqueTimer;
+    [SerializeField]
+    private GameObject button;
+
+	// Use this for initialization
+	void Start () {
+        opaqueTimer = 0.0f;
+        maxOpaqueTimer = 4.0f;
+        BeTransparent();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // already opaque, so no need check
+        if (opaqueTimer <= 0.0f)
+            return;
+
+
+        // When timer is up, go back to transparent
+        opaqueTimer -= Time.deltaTime;
+        if (opaqueTimer < 0.0f)
+            BeTransparent();
+	}
+
+
+
+    public void BeOpaque()
+    {
+        button.GetComponent<CanvasGroup>().DOFade(1.0f, 0.2f);
+        // Set Opaque Timer 
+        opaqueTimer = maxOpaqueTimer;
+    }
+
+    public void BeTransparent()
+    {
+        button.GetComponent<CanvasGroup>().DOFade(0.4f, 0.6f);
+        // Reset Opaque Timer
+        opaqueTimer = 0.0f;
+    }
+}
---
+++ IDProg_A2/Assets/Scripts/Game_Scene/SkillButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void SkillUsed()
+    {
+        Debug.Log("USED!!");
+        // Make all the attacking buttons be opaque as skills have been used
+        gameObject.GetComponentInParent<AttackButton>().BeOpaque();
+    }
+}

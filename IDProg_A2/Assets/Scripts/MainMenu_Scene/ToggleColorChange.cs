c1aa36297c1d6afef9e4ee87967ffa102be92508
JamesCrr <g0546699m@gmail.com>
2019-01-26 15:42:31 +0800

    added color to toggle button

---
+++ IDProg_A2/Assets/Scripts/MainMenu_Scene/ToggleColorChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ToggleColorChange : MonoBehaviour {
+
+    [SerializeField]
+    private ColorBlock selected, notSelected;
+	
+    public void ChangeColor()
+    {
+        if (GetComponent<Toggle>().isOn)
+            GetComponent<Toggle>().colors = selected;
+        else
+            GetComponent<Toggle>().colors = notSelected;
+    }
+}

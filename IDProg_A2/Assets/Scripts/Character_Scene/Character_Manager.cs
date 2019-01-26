eccae2f48348ea23a93de73567bfa94a3ae99b03
gideon <gideonnfrancis999@gmail.com>
2019-01-27 06:23:42 +0800

    Added Status Effects
    I think its probably done now

---
+++ IDProg_A2/Assets/Scripts/Character_Scene/Character_Manager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Manager : MonoBehaviour {
+    //  public string[] StatusEffects = new string[3];
+    public enum StatusEffectTypes
+    {
+        CrippledLeg,
+        Diarrhea,
+        Flu,
+        CripplingDepression,
+        TotalTypes
+    }
+    public List<int> StatusEffects = new List<int>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp("1"))
+        { // Testing
+            if (!AddStatusEffects(0))
+                RemoveStatusEffects(0);
+        }
+        else if (Input.GetKeyUp("2"))
+        { // Testing
+            if (!AddStatusEffects(1))
+                RemoveStatusEffects(1);
+        }
+       else  if (Input.GetKeyUp("3"))
+        { // Testing
+            if (!AddStatusEffects(2))
+                RemoveStatusEffects(2);
+        }
+        else if (Input.GetKeyUp("4"))
+        { // Testing
+            if (!AddStatusEffects(3))
+                RemoveStatusEffects(3);
+        }
+
+    }
+
+    public bool RemoveStatusEffects(int i)
+    {
+        StatusEffects.Remove(i);
+        return true;
+    }
+
+    public bool AddStatusEffects(int i)
+    {
+        //int i = 0;
+        foreach(int it in StatusEffects)
+        {
+            if (it == i)
+            {
+                // It exist in the current list
+                return false;
+            }
+        }
+
+        // It doesnt ecist
+        StatusEffects.Add(i);
+        return true;
+    }
+}
---
+++ IDProg_A2/Assets/Scripts/Character_Scene/StatusEffects_Handler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatusEffects_Handler : MonoBehaviour {
+    private Character_Manager Character;
+    public GameObject StatusText;
+	// Use this for initialization
+	void Start () {
+        Character = GetComponentInParent<Character_Manager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string tempValue = "";
+		foreach(int i in Character.StatusEffects)
+        {
+            switch((Character_Manager.StatusEffectTypes)i)
+            {
+                case Character_Manager.StatusEffectTypes.CrippledLeg:
+                    {
+                        tempValue = tempValue + "Crippled Leg\n";
+                    }
+                    break;
+                case Character_Manager.StatusEffectTypes.Diarrhea:
+                    {
+                        tempValue = tempValue + "Diarrhea\n";
+                    }
+                    break;
+                case Character_Manager.StatusEffectTypes.Flu:
+                    {
+                        tempValue = tempValue + "Flu\n";
+
+                    }
+                    break;
+                case Character_Manager.StatusEffectTypes.CripplingDepression:
+                    {
+                        tempValue = tempValue + "Crippling Depression\n";
+                    }
+                    break;
+            }
+        }
+
+        StatusText.GetComponent<TextMeshProUGUI>().text = tempValue;
+	}
+}

bfc16e7c83d381cc319780d5e2c3f3960c038123
gideon <gideonnfrancis999@gmail.com>
2019-01-27 07:11:13 +0800

    Started on Inventory Screen

---
+++ IDProg_A2/Assets/Scripts/Inventory_Scene/InventorySelectionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventorySelectionHandler : MonoBehaviour {
+    Inventory_Manager InventoryManager;
+    public GameObject[] EquiptmentSlots = new GameObject[(int)Inventory_Manager.EquiptmentTypes.TotalTypes];
+	// Use this for initialization
+	void Start () {
+        InventoryManager.GetComponentInParent<Inventory_Manager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < (int)Inventory_Manager.EquiptmentTypes.TotalTypes; ++i)
+        {
+            //InventoryArray[i] = (int)InventoryManager.InventorySlots[i];
+            //switch(InventoryArray[i])
+            switch(InventoryManager.InventorySlots[i])
+            {
+                case Inventory_Manager.EquiptmentTypes.Weapons:
+                    {
+                        EquiptmentSlots[i].GetComponent<TextMeshProUGUI>().text = "Weapons";
+                    }
+                    break;
+                case Inventory_Manager.EquiptmentTypes.Apparel:
+                    {
+                        EquiptmentSlots[i].GetComponent<TextMeshProUGUI>().text = "Apparel";
+                    }
+                    break;
+
+                case Inventory_Manager.EquiptmentTypes.Potions:
+                    {
+                        EquiptmentSlots[i].GetComponent<TextMeshProUGUI>().text = "Potions";
+                    }
+                    break;
+
+                case Inventory_Manager.EquiptmentTypes.Misc:
+                    {
+                        EquiptmentSlots[i].GetComponent<TextMeshProUGUI>().text = "Misc";
+                    }
+                    break;
+
+            }
+        }
+	}
+}
---
+++ IDProg_A2/Assets/Scripts/Inventory_Scene/Inventory_Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_Manager : MonoBehaviour {
+    public enum EquiptmentTypes
+    {
+        Weapons,
+        Apparel,
+        Potions,
+        Misc,
+        TotalTypes
+    }
+    public GameObject Arrow;
+    // An array to hold the number of active slots
+    public EquiptmentTypes[] InventorySlots = new EquiptmentTypes[(int)EquiptmentTypes.TotalTypes];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShiftUp()
+    {
+        EquiptmentTypes temp = InventorySlots[0]; // Store the top element
+        int i;
+        for (i = 0; i < ((int)EquiptmentTypes.TotalTypes - 1); ++i)
+        {
+            // Swap the current element with the next one
+            InventorySlots[i] = InventorySlots[i + 1];
+        }
+        InventorySlots[i] = temp; // Change the last element to the first one
+    }
+
+    public void ShiftDown()
+    {
+        EquiptmentTypes temp = InventorySlots[(int)EquiptmentTypes.TotalTypes - 1]; // Store the last element into a temp
+        int i;
+        for (i = (int)EquiptmentTypes.TotalTypes - 1; i > 0; --i)
+        {
+            InventorySlots[i] = InventorySlots[i - 1];
+        }
+        InventorySlots[i] = temp;
+    }
+}

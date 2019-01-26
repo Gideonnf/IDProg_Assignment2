707e26ec8942469b37b199c51d3d327576c46687
gideon <gideonnfrancis999@gmail.com>
2019-01-27 05:39:39 +0800

    Forgot to put description

---
+++ IDProg_A2/Assets/Scripts/Character_Scene/Display_Buttons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display_Buttons : MonoBehaviour {
+    public Attribute_Manager.Stats stat;
+    private Display_Manager DisplayManager;
+	// Use this for initialization
+	void Start () {
+        DisplayManager = GetComponentInParent<Display_Manager>();
+	}
+	// This can be done without using a separate script
+    // But just having a button with an int pass in and call that
+    // on the button press but I might need to use this in the future
+    // so im doing this here incase
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void Show()
+    {
+        DisplayManager.ShowDescription(stat);
+    }
+}
--- IDProg_A2/Assets/Scripts/Character_Scene/Display_Manager.cs
+++ IDProg_A2/Assets/Scripts/Character_Scene/Display_Manager.cs
@@ -8,6 +8,12 @@ public class Display_Manager : MonoBehaviour {
     private Attribute_Manager Attribute_Manager;
     private int childCount = 0;
     private GameObject[] Displayed_Attributes;
+    private bool[] DisplayedDescription = new bool[(int)Attribute_Manager.Stats.Total_Stats];
+    private bool isDisplaying = false;
+    public GameObject Description;
+    public GameObject DescriptionButton;
+    public Sprite[] SpriteDescription = new Sprite[(int)Attribute_Manager.Stats.Total_Stats];
+  
 	// Use this for initialization
 	void Start () {
         int offset = 0;  // for offsetting the array
@@ -41,16 +47,67 @@ public class Display_Manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		for(int i = 0; i < (int)Attribute_Manager.Stats.Total_Stats; ++i)
+        if(isDisplaying == false)
         {
-           if (Attribute_Manager.Modifier[i] > 0)
+            for (int i = 0; i < (int)Attribute_Manager.Stats.Total_Stats; ++i)
             {
-                Displayed_Attributes[i].GetComponentInChildren<TextMeshProUGUI>().text = Attribute_Manager.Attributes[i].ToString() + " (+" + Attribute_Manager.Modifier[i] + ")";
-            }
-           else if (Attribute_Manager.Modifier[i] == 0)
-            {
-                Displayed_Attributes[i].GetComponentInChildren<TextMeshProUGUI>().text = Attribute_Manager.Attributes[i].ToString();
+                if (Attribute_Manager.Modifier[i] > 0)
+                {
+                    Displayed_Attributes[i].GetComponentInChildren<TextMeshProUGUI>().text = Attribute_Manager.Attributes[i].ToString() + " (+" + Attribute_Manager.Modifier[i] + ")";
+                }
+                else if (Attribute_Manager.Modifier[i] == 0)
+                {
+                    Displayed_Attributes[i].GetComponentInChildren<TextMeshProUGUI>().text = Attribute_Manager.Attributes[i].ToString();
+                }
             }
         }
-	}
+    }
+
+    public void ShowDescription(Attribute_Manager.Stats stats)
+    {
+        for (int i = 0; i < (int)Attribute_Manager.Stats.Total_Stats; ++i)
+        { // Reset all the displayed to false;
+            if (DisplayedDescription[i] == true)
+                DisplayedDescription[i] = false;
+
+            //Disable the button objects
+            Displayed_Attributes[i].GetComponent<Button>().enabled = false;
+        }
+
+        //Enables the description box and changes the sprite
+        Description.GetComponent<Image>().enabled = true;
+        Description.GetComponent<Image>().sprite = SpriteDescription[(int)stats];
+        DescriptionButton.GetComponent<Image>().enabled = true;
+        DescriptionButton.GetComponent<Button>().enabled = true;
+        //Doesnt work
+        Description.GetComponentInChildren<Button>().enabled = true;
+        Description.GetComponentInChildren<Image>().enabled = true;
+
+        // Set the boolean to true
+        DisplayedDescription[(int)stats] = true;
+        isDisplaying = true;
+    }
+
+    public void CloseDescription()
+    {
+        for(int i = 0; i < (int)Attribute_Manager.Stats.Total_Stats; ++i)
+        {
+            // Reset all the boolean to false
+            if (DisplayedDescription[i] == true)
+                DisplayedDescription[i] = false;
+
+            //Enable the button objects again
+            Displayed_Attributes[i].GetComponent<Button>().enabled = true;
+        }
+
+        Description.GetComponent<Image>().enabled = false;
+        DescriptionButton.GetComponent<Image>().enabled = false;
+        DescriptionButton.GetComponent<Button>().enabled = false;
+
+        //Doesnt work also
+        Description.GetComponentInChildren<Button>().enabled = false;
+        Description.GetComponentInChildren<Image>().enabled = false;
+
+        isDisplaying = false;
+    }
 }

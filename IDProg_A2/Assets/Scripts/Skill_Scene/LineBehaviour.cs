65f0950abf2e152cd3aec4ea8e46de61906723ac
Jordan Tan Yan Boon <31056129+174561B@users.noreply.github.com>
2019-01-28 14:27:35 +0800

    Line now light up

--- IDProg_A2/Assets/Scripts/Skill_Scene/LineBehaviour.cs
+++ IDProg_A2/Assets/Scripts/Skill_Scene/LineBehaviour.cs
@@ -8,27 +8,60 @@ public class LineBehaviour : MonoBehaviour {
     public Image MyLine;
     public Button To;
     public Button From;
-    public Image Glow;
-    public Image NoGlow;
+    public Sprite Glow;
+    public Sprite NoGlow;
 
 	// Use this for initialization
-	void Start () {		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        //if (To.GetComponent<ButtonHandlerTrio>() != null && From.GetComponent<ButtonHandlerOrigin>() != null)
-        //{
-        //    if (To.GetComponent<ButtonHandlerTrio>().GetActive() && From.GetComponent<ButtonHandlerOrigin>().GetActive())
-        //    {
-        //        MyLine = Glow;
-        //    }
-        //    else
-        //    {
-        //        MyLine = NoGlow;
-        //    }
-        //
-        //}
+        if (To.GetComponent<ButtonHandlerTrio>() != null && From.GetComponent<ButtonHandlerOrigin>() != null)
+        {
+            if (To.GetComponent<ButtonHandlerTrio>().GetActive() && From.GetComponent<ButtonHandlerOrigin>().GetActive())
+            {
+                MyLine.sprite = Glow;
+            }
+            else
+            {
+                MyLine.sprite = NoGlow;
+            }
+        }
+        else if (To.GetComponent<ButtonHandlerTrio>() != null && From.GetComponent<ButtonHandlerTrio>() != null)
+        {
+            if (To.GetComponent<ButtonHandlerTrio>().GetActive() && From.GetComponent<ButtonHandlerTrio>().GetActive())
+            {
+                MyLine.sprite = Glow;
+            }
+            else
+            {
+                MyLine.sprite = NoGlow;
+            }
+        }
+        else if (To.GetComponent<ButtonHandlerSolo>() != null && From.GetComponent<ButtonHandlerTrio>() != null)
+        {
+            if (To.GetComponent<ButtonHandlerSolo>().GetActive() && From.GetComponent<ButtonHandlerTrio>().GetActive())
+            {
+                MyLine.sprite = Glow;
+            }
+            else
+            {
+                MyLine.sprite = NoGlow;
+            }
+        }
+        else if (To.GetComponent<ButtonHandlerDuo>() != null && From.GetComponent<ButtonHandlerTrio>() != null)
+        {
+            if (To.GetComponent<ButtonHandlerDuo>().GetActive() && From.GetComponent<ButtonHandlerTrio>().GetActive())
+            {
+                MyLine.sprite = Glow;
+            }
+            else
+            {
+                MyLine.sprite = NoGlow;
+            }
+        }
     }
 }

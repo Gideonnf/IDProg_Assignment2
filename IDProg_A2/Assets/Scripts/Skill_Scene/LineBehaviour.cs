7ff12caf8884bcf1b6777e3869d711c722e128aa
Jordan Tan Yan Boon <31056129+174561B@users.noreply.github.com>
2019-01-28 12:02:00 +0800

    Started on line glow

---
+++ IDProg_A2/Assets/Scripts/Skill_Scene/LineBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineBehaviour : MonoBehaviour {
+
+    public Image MyLine;
+    public Button To;
+    public Button From;
+    public Image Glow;
+    public Image NoGlow;
+
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (To.GetComponent<ButtonHandlerTrio>() != null && From.GetComponent<ButtonHandlerOrigin>() != null)
+        //{
+        //    if (To.GetComponent<ButtonHandlerTrio>().GetActive() && From.GetComponent<ButtonHandlerOrigin>().GetActive())
+        //    {
+        //        MyLine = Glow;
+        //    }
+        //    else
+        //    {
+        //        MyLine = NoGlow;
+        //    }
+        //
+        //}
+    }
+}
